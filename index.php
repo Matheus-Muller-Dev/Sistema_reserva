efee4fce1a3852279ba2c8dbb05bab33ff2c11a5
Matheus MW <matheusmiiller44@gmail.com>
2025-06-02 00:18:14 -0300

    Foi modificado alguns arquivos de lugar e adicionado um para o '.gitignore'

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <?php
 // C:\xampp\php\php.exe index.php
 
-include "Banco.php";
+include "banco/Banco.php";
+include "funcoes/listarQuartoDisponveis.php";
+include "funcoes/listarDescricoesQuarto.php";
 
 $conexao = dbx_connecta("localhost", "root", "", "reservamm");
 
 echo "Pousada Müller! Controle de hospedes e quartos! \n";
 
-$nomeCliente = readline("Como é o seu nome? \n");
+echo "Selecione uma opção: \n";
+echo "1. Reservar um quarto. \n";
+echo "2. Descrições dos quartos. \n";
+echo "3. Cadastrar um cliente. \n";
+echo "4. Cadastrar um novo quarto. \n";
+$opcoesSelecionada = readline(" ");
 
-$sql = "INSERT INTO clientes (nome) VALUES ('$nomeCliente')";
-
-if (mysqli_query($conexao, $sql)) {
-    echo "Cliente salvo com sucesso!";
+if ($opcoesSelecionada == 1) {
+    listarQuartosDisponiveis($conexao);
+} elseif ($opcoesSelecionada == 2) {
+    listarDescricoesQuarto($conexao);
 } else {
-    echo "Erro ao salvar cliente: " . mysqli_error($conexao);
-}
+    echo "Opção invalida, digite apenas números da seleção. \n";
+} 
 
-mysqli_close($conexao);
 ?>
